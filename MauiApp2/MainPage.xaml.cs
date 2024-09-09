f6312df2afaa2830fb1bb3dbeec71df5dd5854b9
REzzK0 <anadaminus@gmail.com>
2024-09-09 17:36:03 +1000

    Добавьте файлы проекта.

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        private string _currentEntry=string.Empty;
+        private string _operator = string.Empty;
+        private double _value = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ClearButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CE_ButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void C_ButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PlusMinusButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SquareButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumberButtonClicked(object sender, EventArgs e)
+        {
+           var button= sender as Button;
+            _currentEntry += button.Text;
+            Display.Text = _currentEntry;
+           
+        }
+
+        private void CommaButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EqualsButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void XButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PercentButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OperatorButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
